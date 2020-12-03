a18180c99888a76bcbb4b1a864e696ad92254e79
cstringer17 <callumstringer10@gmail.com>
2020-12-03 15:07:10 +0100

    Day 2

---
+++ aoc-PasswordPhilosophy/passwordPhilosophyP1.ps1
@@ -0,0 +1,32 @@
+﻿[string[]]$arrayFromFile = Get-Content -Path 'D:\Programming\aoc\aoc-PasswordPhilosophy\input.txt' 
+$goodpasswords = 0
+$badpasswords = 0
+Foreach ($line in $arrayFromFile)
+{
+    $partLine = $line.Split(" ")
+
+    $minmax = $partLine[0].Split("-")
+    $letter = $partLine[1].ToCharArray()[0]
+    $password = $partLine[2]
+    
+    $passwordArray = $password.ToCharArray();
+    $letterCounter = 0;
+
+    Foreach ($char in $passwordArray)
+    {
+    if($char -eq $letter)
+        {
+        $letterCounter++
+        }
+    }
+    $min = $minmax[0] -as [int]
+    $max = $minmax[1] -as [int]
+
+    if($min -le $letterCounter -And $max -ge $letterCounter){
+        $goodpasswords++
+    }else{
+        $badpasswords++
+    }
+}
+
+Write-Output $goodpasswords
---
+++ aoc-PasswordPhilosophy/passwordPhilosophyP2.ps1
@@ -0,0 +1,44 @@
+﻿[string[]]$arrayFromFile = Get-Content -Path 'D:\Programming\aoc\aoc-PasswordPhilosophy\input.txt' 
+$goodpasswords = 0
+$badpasswords = 0
+Foreach ($line in $arrayFromFile)
+{
+    $partLine = $line.Split(" ")
+
+    $minmax = $partLine[0].Split("-")
+    $letter = $partLine[1].ToCharArray()[0]
+    $password = $partLine[2]
+    
+    $passwordArray = $password.ToCharArray();
+    $letterCounter = 0;
+
+    
+    $min = $minmax[0] -as [int]
+    $max = $minmax[1] -as [int]
+
+  
+    if($passwordArray[$min-1] -like $letter -And $passwordArray[$max-1] -notlike $letter  ){
+        $goodpasswords++
+        Write-Output $passwordArray[$min] 
+        Write-Output $passwordArray[$max]
+        Write-Output $letter
+        
+    }else{
+        $badpasswords++
+    }
+
+    if($passwordArray[$min-1] -notlike $letter -And $passwordArray[$max-1] -like $letter  ){
+        $goodpasswords++
+        Write-Output $passwordArray[$min] 
+        Write-Output $passwordArray[$max]
+        Write-Output $letter
+        
+    }else{
+        $badpasswords++
+    }
+
+
+    Write-Output "------"
+}
+
+Write-Output $goodpasswords
